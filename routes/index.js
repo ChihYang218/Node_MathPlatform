4264663ed5f9b64d15934446e62720f30d6f7ca4
ChihYang218 <chihyang218@gmail.com>
2023-10-20 19:56:22 +0800

    以 CDN 方式抓取 google font

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '專案首頁' });
 });
 
 module.exports = router;
