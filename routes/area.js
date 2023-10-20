32a09c0c6555e86f24b038a62b53e244a210e1af
ChihYang218 <chihyang218@gmail.com>
2023-10-20 20:26:28 +0800

    初始化 基本功能頁面

--- routes/area.js
+++ routes/area.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('area', { title: 'Express' });
+  res.render('area', { title: '面積計算' });
 });
 
 module.exports = router;
--- routes/volume.js
+++ routes/volume.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('volume', { title: 'Express' });
+  res.render('volume', { title: '體積計算' });
 });
 
 module.exports = router;
