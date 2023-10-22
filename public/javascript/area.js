746144472969655e4377389d8dfa783ecf0a301b
ChihYang218 <chihyang218@gmail.com>
2023-10-22 17:47:47 +0800

    解決 計算區塊 輸入空值或是負數的問題

--- public/javascript/area.js
+++ public/javascript/area.js
@@ -8,6 +8,17 @@ $(function() {
       document.getElementById('area-init').innerHTML = "<div class='col3'><table><tr><th>邊長(cm): </th><td><div class='form-outline' style='width: 10rem;'><input step='0.01' value='0.00' type='number' id='number1' class='form-control' /></div></td></tr><tr><th>面積(cm²): </th><td><div class='form-outline' style='width: 10rem;'><input type='text' class='form-control' placeholder='等待結果' id='result'></div></td></tr></table><button type='button' class='btn btn-warning btn-lg fnchoice' id='countBtn-area'>計算面積</button></div>";
       $('#countBtn-area').click(() => {
         let num = $('#number1').val();
+        if(num == '')
+        {
+          alert('請輸入計算值！');
+          return;
+        }
+        let buffer = parseFloat(num);
+        if(buffer < 0)
+        {
+          alert('請輸入正數！');
+          return;
+        }
         $.post(
           '/area/s/' + num,
           (data) => {
@@ -22,6 +33,18 @@ $(function() {
       $('#countBtn-area').click(() => {
         let num1 = $('#number1').val();
         let num2 = $('#number2').val();
+        if(num1 == '' || num2 == '')
+        {
+          alert('請輸入計算值！');
+          return;
+        }
+        let buffer1 = parseFloat(num1);
+        let buffer2 = parseFloat(num2);
+        if(buffer1 < 0 || buffer2 < 0)
+        {
+          alert('請輸入正數！');
+          return;
+        }
         $.post(
           '/area/r/' + num1 + '/' + num2,
           (data) => {
@@ -35,6 +58,17 @@ $(function() {
       document.getElementById('area-init').innerHTML = "<div class='col3'><table><tr><th>圓半徑長(cm): </th><td><div class='form-outline' style='width: 10rem;'><input step='0.01' value='0.00' type='number' id='number1' class='form-control' /></div></td></tr><tr><th>面積(cm²): </th><td><div class='form-outline' style='width: 10rem;'><input type='text' class='form-control' placeholder='等待結果' id='result'></div></td></tr></table><button type='button' class='btn btn-warning btn-lg fnchoice' id='countBtn-area'>計算面積</button></div>";
       $('#countBtn-area').click(() => {
         let num = $('#number1').val();
+        if(num == '')
+        {
+          alert('請輸入計算值！');
+          return;
+        }
+        let buffer = parseFloat(num);
+        if(buffer < 0)
+        {
+          alert('請輸入正數！');
+          return;
+        }
         $.post(
           '/area/c/' + num,
           (data) => {
@@ -49,7 +83,5 @@ $(function() {
       alert('請先選擇圖形！');
       return;
     }
-
-
   });
 })
