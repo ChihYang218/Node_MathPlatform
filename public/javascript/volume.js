ac7ef2dcfe14073dcb21fe12a9760b4903e261ec
ChihYang218 <chihyang218@gmail.com>
2023-10-22 18:03:11 +0800

    加入正方形體積計算的功能

--- public/javascript/volume.js
+++ public/javascript/volume.js
@@ -5,6 +5,29 @@ $(function() {
     if(shape == 's')
     {
       document.getElementById('volume-init').innerHTML = "<div class='col3'><table><tr><th>邊長(cm): </th><td><div class='form-outline' style='width: 10rem;'><input step='0.01' value='0.00' type='number' id='number1' class='form-control' /></div></td></tr><tr><th>體積(cm³): </th><td><div class='form-outline' style='width: 10rem;'><input type='text' class='form-control' placeholder='等待結果' id='result'></div></td></tr></table><button type='button' class='btn btn-warning btn-lg fnchoice' id='countBtn-volume'>計算體積</button></div>";
+      $('#countBtn-volume').click(() => {
+        let num = $('#number1').val();
+        if(num == '')
+        {
+          alert('請輸入計算值！');
+          return;
+        }
+        let buffer = parseFloat(num);
+        if(buffer < 0)
+        {
+          alert('請輸入正數！');
+          return;
+        }
+        $.ajax({
+          url: '/volume/s',
+          method: 'POST',
+          data: {num: num},
+          datatype: 'json',
+          success: (data) => {
+            $('#result').val(data.area);
+          }
+        });
+      })
     }
     else if(shape == 'r')
     {
--- routes/volume.js
+++ routes/volume.js
@@ -5,4 +5,15 @@ router.get('/', function(req, res, next) {
   res.render('volume', { title: '體積計算' });
 });
 
+router.post('/s', (req, res) => {
+  let num = req.body.num;
+  let area;
+
+  num = parseFloat(num);
+  area = Math.pow(num, 3);
+  area = area.toFixed(2);
+
+  res.send({area: area});
+})
+
 module.exports = router;
