1325ad0cd57ef1f7b8cd379533bc5ea9da4534d6
ChihYang218 <chihyang218@gmail.com>
2023-10-22 16:20:14 +0800

    體積計算頁面，加入選擇圖形跳出相對應要計算的介面
    此邏輯很簡單，直接判斷是什麼形，innerHTML相對應的介面，要是沒有選擇的話就是空的HTML
    
    每次都要判斷有沒有選擇圖形，這樣才會收起要計算的介面

--- public/javascript/volume.js
+++ public/javascript/volume.js
@@ -1,9 +1,22 @@
 $(function() {
+  let shape;
   $('#choiceFinishBtn-volume').click(() => {
-    let choice = $('#choiceShape-volume').val();
-
-    if(choice != '1' && choice != '2' && choice != '3')
+    let shape = $('#choiceShape-volume').val();
+    if(shape == 's')
     {
+      document.getElementById('volume-init').innerHTML = "<div class='col3'><table><tr><th>邊長(cm): </th><td><div class='form-outline' style='width: 10rem;'><input step='0.01' value='0.00' type='number' id='number1' class='form-control' /></div></td></tr><tr><th>體積(cm³): </th><td><div class='form-outline' style='width: 10rem;'><input type='text' class='form-control' placeholder='等待結果' id='result'></div></td></tr></table><button type='button' class='btn btn-warning btn-lg fnchoice' id='countBtn-volume'>計算面積</button></div>";
+    }
+    else if(shape == 'r')
+    {
+      document.getElementById('volume-init').innerHTML = "<div class='col3'><table><tr><th>長(cm): </th><td><div class='form-outline' style='width: 10rem;'><input step='0.01' value='0.00' type='number' id='number1' class='form-control' /></div></td></tr><tr><th>寬(cm): </th><td><div class='form-outline' style='width: 10rem;'><input step='0.01' value='0.00' type='number' id='number2' class='form-control' /></div></td></tr><tr><th>高(cm): </th><td><div class='form-outline' style='width: 10rem;'><input step='0.01' value='0.00' type='number' id='number2' class='form-control' /></div></td></tr><tr><th>體積(cm³): </th><td><div class='form-outline' style='width: 10rem;'><input type='text' class='form-control' placeholder='等待結果' id='result'></div></td></tr></table><button type='button' class='btn btn-warning btn-lg fnchoice' id='countBtn-volume'>計算面積</button></div>";
+    }
+    else if(shape == 'c')
+    {
+      document.getElementById('volume-init').innerHTML = "<div class='col3'><table><tr><th>圓半徑長(cm): </th><td><div class='form-outline' style='width: 10rem;'><input step='0.01' value='0.00' type='number' id='number1' class='form-control' /></div></td></tr><tr><th>體積(cm³): </th><td><div class='form-outline' style='width: 10rem;'><input type='text' class='form-control' placeholder='等待結果' id='result'></div></td></tr></table><button type='button' class='btn btn-warning btn-lg fnchoice' id='countBtn-volume'>計算面積</button></div>";
+    }
+    else
+    {
+      document.getElementById('volume-init').innerHTML = "";
       alert('請先選擇圖形！');
       return;
     }
