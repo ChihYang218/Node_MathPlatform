af8f35c5439a901bace220ccacf5f608605e5641
ChihYang218 <chihyang218@gmail.com>
2023-10-22 15:53:49 +0800

    面積計算頁面，加入選擇圖形跳出相對應要計算的介面
    此邏輯很簡單，直接判斷是什麼形，innerHTML相對應的介面，要是沒有選擇的話就是空的HTML
    
    每次都要判斷有沒有選擇圖形，這樣才會收起要計算的介面

--- public/javascript/area.js
+++ public/javascript/area.js
@@ -1,9 +1,23 @@
 $(function() {
+  let shape;
   $('#choiceFinishBtn-area').click(() => {
-    let choice = $('#choiceShape-area').val();
+    let shape = $('#choiceShape-area').val();
 
-    if(choice != '1' && choice != '2' && choice != '3')
+    if(shape == 's')
     {
+      document.getElementById('area-init').innerHTML = "<div class='col3'><table><tr><th>邊長(cm): </th><td><div class='form-outline' style='width: 10rem;'><input step='0.01' value='0.00' type='number' id='number1' class='form-control' /></div></td></tr><tr><th>面積(cm²): </th><td><div class='form-outline' style='width: 10rem;'><input type='text' class='form-control' placeholder='等待結果' id='result'></div></td></tr></table><button type='button' class='btn btn-warning btn-lg fnchoice' id='countBtn-area'>計算面積</button></div>";
+    }
+    else if(shape == 'r')
+    {
+      document.getElementById('area-init').innerHTML = "<div class='col3'><table><tr><th>長(cm): </th><td><div class='form-outline' style='width: 10rem;'><input step='0.01' value='0.00' type='number' id='number1' class='form-control' /></div></td></tr><tr><th>寬(cm): </th><td><div class='form-outline' style='width: 10rem;'><input step='0.01' value='0.00' type='number' id='number2' class='form-control' /></div></td></tr><tr><th>面積(cm²): </th><td><div class='form-outline' style='width: 10rem;'><input type='text' class='form-control' placeholder='等待結果' id='result'></div></td></tr></table><button type='button' class='btn btn-warning btn-lg fnchoice' id='countBtn-area'>計算面積</button></div>";
+    }
+    else if(shape == 'c')
+    {
+      document.getElementById('area-init').innerHTML = "<div class='col3'><table><tr><th>圓半徑長(cm): </th><td><div class='form-outline' style='width: 10rem;'><input step='0.01' value='0.00' type='number' id='number1' class='form-control' /></div></td></tr><tr><th>面積(cm²): </th><td><div class='form-outline' style='width: 10rem;'><input type='text' class='form-control' placeholder='等待結果' id='result'></div></td></tr></table><button type='button' class='btn btn-warning btn-lg fnchoice' id='countBtn-area'>計算面積</button></div>";
+    }
+    else
+    {
+      document.getElementById('area-init').innerHTML = "";
       alert('請先選擇圖形！');
       return;
     }
